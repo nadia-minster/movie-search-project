5589fc564f0c1d2b08d5d839d3f0c89e55ce55d2
nadia <petrovanadia96@gmail.com>
2023-05-01 20:47:30 +1000

    responsive navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,18 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
+import { useMediaQuery } from "react-responsive";
+import { FaBars, FaTimes } from "react-icons/fa";
 import logo from "./assets/images/logo.png";
 
 const Navbar = () => {
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
+  const toggleMobileMenu = () => {
+    setShowMobileMenu(!showMobileMenu);
+  };
+  const [isOpen, setIsOpen] = useState(false);
+  const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
+
   return (
     <section className="main-navbar main">
       <img src={logo} alt="logo" />
-      <nav className="navigation">
-        <ul>
-          <li>About Us</li>
-          <li>Contact</li>
-          <li>Saved Movies</li>
-          <button className="btn">Login</button>
-        </ul>
-      </nav>
+      {!isMobile ? (
+        <nav className="navigation">
+          <ul>
+            <li>About Us</li>
+            <li>Contact</li>
+            <li>Saved Movies</li>
+            <button className="btn">Login</button>
+          </ul>
+        </nav>
+      ) : (
+        <div class="screen-2">
+          <div class="hamburger hb-2">
+            {!isOpen ? (
+              <FaBars onClick={() => setIsOpen(!isOpen)} />
+            ) : (
+              <FaTimes onClick={() => setIsOpen(!isOpen)} />
+            )}
+          </div>
+          <div class="content">
+            <nav className={`${isOpen && "active"}`}>
+              <ul>
+                <li>LINK 1</li>
+                <li>LINK 2</li>
+                <li>LINK 3</li>
+                <li>LINK 4</li>
+                <li>LINK 5</li>
+                <li>LINK 6</li>
+              </ul>
+            </nav>
+            <div class="container-2"></div>
+          </div>
+        </div>
+      )}
     </section>
   );
 };

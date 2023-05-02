3989bce771fc6d6c5bb6868ec98c767bf9cfe62a
nadia <petrovanadia96@gmail.com>
2023-05-02 19:52:52 +1000

    edited home page

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Home = () => {
-  return <div className="main home-main">Home</div>;
+  return (
+    <div className="main home-main">
+      <h1>Can't pick a movie?</h1>
+      <p>
+        Find your perfect movie in seconds! Choose a year and genre, and we'll
+        show you three top picks to match your taste. Say goodbye to endless
+        scrolling and hello to movie night done right!
+      </p>
+      <button className="btn">Begin your search</button>
+    </div>
+  );
 };
 
 export default Home;
---
+++ src/hooks/useFetch.js
@@ -0,0 +1,20 @@
+import { useState, useEffect } from "react";
+import axiow from "axios";
+
+const useFetch = (year, genre) => {
+  const [movies, setMovies] = useState([]);
+  const apiKey = import.meta.env.VITE_API_KEY;
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      const response = await axios.get(
+        `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US&sort_by=popularity.desc&page=1&primary_release_year=${year}&with_genres=${genre}`
+      );
+      setMovies(response.data.results);
+    };
+    fetchMovies();
+  }, [year, genre]);
+  return movies;
+};
+
+export default useFetch;

5e16498fb7ae973bababb835b27b6fd245eda564
nadia <petrovanadia96@gmail.com>
2023-05-02 14:32:14 +1000

    edited mobile menu

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -4,47 +4,43 @@ import { FaBars, FaTimes } from "react-icons/fa";
 import logo from "./assets/images/logo.png";
 
 const Navbar = () => {
-  const [showMobileMenu, setShowMobileMenu] = useState(false);
-  const toggleMobileMenu = () => {
-    setShowMobileMenu(!showMobileMenu);
-  };
   const [isOpen, setIsOpen] = useState(false);
   const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
+
+  const NavigationStyling = () => {
+    if (isMobile && isOpen) {
+      return "links-mobile links-active";
+    } else if (isMobile) {
+      return "links-mobile";
+    } else return "links";
+  };
+
+  const navigation = (
+    <nav className={`${isOpen && isMobile ? "active" : "navigation"}`}>
+      <ul className={NavigationStyling()}>
+        <li>About Us</li>
+        <li>Contact</li>
+        <li>Saved Movies</li>
+        <button className="btn">Login</button>
+      </ul>
+    </nav>
+  );
 
   return (
     <section className="main-navbar main">
       <img src={logo} alt="logo" />
       {!isMobile ? (
-        <nav className="navigation">
-          <ul>
-            <li>About Us</li>
-            <li>Contact</li>
-            <li>Saved Movies</li>
-            <button className="btn">Login</button>
-          </ul>
-        </nav>
+        navigation
       ) : (
-        <div class="screen-2">
-          <div class="hamburger hb-2">
+        <div>
+          <div class="hamburger hb-2" onClick={() => setIsOpen(!isOpen)}>
             {!isOpen ? (
-              <FaBars onClick={() => setIsOpen(!isOpen)} />
+              <FaBars className="icon" />
             ) : (
-              <FaTimes onClick={() => setIsOpen(!isOpen)} />
+              <FaTimes className="icon" />
             )}
           </div>
-          <div class="content">
-            <nav className={`${isOpen && "active"}`}>
-              <ul>
-                <li>LINK 1</li>
-                <li>LINK 2</li>
-                <li>LINK 3</li>
-                <li>LINK 4</li>
-                <li>LINK 5</li>
-                <li>LINK 6</li>
-              </ul>
-            </nav>
-            <div class="container-2"></div>
-          </div>
+          <div class="mobile-menu">{navigation}</div>
         </div>
       )}
     </section>

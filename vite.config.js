b010752f3ffc833cf58f518be5393f0963a168ab
nadia <petrovanadia96@gmail.com>
2023-06-12 23:13:05 +1000

    removed favicon config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { ViteFaviconsPlugin } from "vite-plugin-favicon2";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    ViteFaviconsPlugin("./assets/favicon_io/favicon-192x192.png"),
-    react(),
-  ],
+  plugins: [react()],
 });

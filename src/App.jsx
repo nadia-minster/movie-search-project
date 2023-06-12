636ab3c52f6d91975cff97934c15d29a828449be
nadia <petrovanadia96@gmail.com>
2023-06-12 23:10:15 +1000

    favicon another fix

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,24 @@ function App() {
       <HelmetProvider>
         <Helmet>
           <title>Movie Picker</title>
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="./assets/favicon_io/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="./assets/favicon_io/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="./assets/favicon_io/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
         </Helmet>
       </HelmetProvider>
 

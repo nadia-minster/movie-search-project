68b3fc411e01a9ab693b335a5d3f6b0915455147
nadia <petrovanadia96@gmail.com>
2023-06-12 22:20:57 +1000

    favicon fix

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { ViteFaviconsPlugin } from "vite-plugin-favicon2";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    ViteFaviconsPlugin("./public/favicon_io/favicon-192x192.png"),
+    ViteFaviconsPlugin("./assets/favicon_io/favicon-192x192.png"),
     react(),
   ],
 });
